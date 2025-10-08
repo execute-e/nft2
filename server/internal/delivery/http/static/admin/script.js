c7f799fdcd2df66e41caf267c17abafc31566f93
scrameee <itsxomyak@gmail.com>
2025-10-08 18:57:18 +0500

    fix: admin panel

--- server/internal/delivery/http/static/admin/script.js
+++ server/internal/delivery/http/static/admin/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const API_BASE_URL = '${import.meta.env.VITE_API_BASE_URL}/admin'
+	const API_BASE_URL = `https://the-monicorns-production.up.railway.app/admin` //  TODO: заменить на реальный урл
 	const tokenInput = document.getElementById('admin-token')
 	const saveTokenBtn = document.getElementById('save-token')
 	const statusMessage = document.getElementById('status-message')
