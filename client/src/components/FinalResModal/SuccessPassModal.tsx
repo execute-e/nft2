5f1476257ff3c6797ebdc10ae79e570db61dd1ae
scrameee <itsxomyak@gmail.com>
2025-10-09 22:13:37 +0500

    fix: share component v2

--- client/src/components/FinalResModal/SuccessPassModal.tsx
+++ client/src/components/FinalResModal/SuccessPassModal.tsx
@@ -14,25 +14,25 @@ type ModalProps = {
 }
 
 const SuccessWindow = ({ isOpen, onClose }: ModalProps) => {
-	const handleShare = () => {
-		const url = 'https://www.monicorns.xyz/'
-		const artURL = 'https://tenor.com/mLMbxxQY9eS.gif'
+const handleShare = () => {
+	const artURL = 'https://tenor.com/mLMbxxQY9eS.gif'
 
-		const text = `Gmoni
+	// 1. Убираем artURL из текста
+	const text = `Gmoni
 
 I just took part in the raffle @the_monicorns on the whitelist 
 
-Participate too ${url}
+Participate too https://www.monicorns.xyz
 
 Monicorns are everywhere`
 
-		// 2. Кодируем текст и URL отдельно и добавляем оба параметра в ссылку
-		const twitterIntentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-			text
-		)}&url=${encodeURIComponent(artURL)}`
+	// 2. Кодируем текст и URL отдельно и добавляем оба параметра в ссылку
+	const twitterIntentUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+		text
+	)}&url=${encodeURIComponent(artURL)}`
 
-		window.open(twitterIntentUrl, '_blank')
-	}
+	window.open(twitterIntentUrl, '_blank')
+}
 
 	useEffect(() => {
 		const handleKeyDown = (e: KeyboardEvent) => {
