7bfaeb8f201a05d16a318577dd906c6d3945f667
scrameee <itsxomyak@gmail.com>
2025-10-18 01:07:24 +0500

    fix: old logic with twitter in client v2

--- client/src/components/RaffleForm/RaffleForm.tsx
+++ client/src/components/RaffleForm/RaffleForm.tsx
@@ -31,7 +31,6 @@ export const RaffleForm = ({ imageUrl, onSubmitSuccess }: RaffleFormProps) => {
 		handleSubmit,
 		formState: { errors, isSubmitting },
 		reset,
-		watch,
 	} = useForm<FormInputs>({
 		defaultValues: initialValues,
 	})
