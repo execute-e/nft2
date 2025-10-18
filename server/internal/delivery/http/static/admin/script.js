24e4bd18e38ecd6e1cb035e8d4bc76d5d4a974ec
scrameee <itsxomyak@gmail.com>
2025-10-18 22:28:54 +0500

    fix: trying to fix admin panel

--- server/internal/delivery/http/static/admin/script.js
+++ server/internal/delivery/http/static/admin/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const API_BASE_URL = 'http://localhost:8080/admin'
+	const API_BASE_URL = '/admin'
 	const tokenInput = document.getElementById('admin-token')
 	const saveTokenBtn = document.getElementById('save-token')
 	const statusMessage = document.getElementById('status-message')
